8aae9817fc4571bd88deb67c8f848c558307fa3b
Janik Brüll <j.bruell@topsystem.de>
2018-10-04 12:03:19 +0200

    [Base] Use correct sensor mode and remove cast in TestLightLevel
    Additionally do not use the behavior pattern anymore.

--- src/robot/TestLightLevel.java
+++ src/robot/TestLightLevel.java
@@ -1,71 +1,34 @@
 package robot;
 
-import lejos.hardware.lcd.LCD;
 import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.SampleProvider;
-import lejos.robotics.subsumption.Arbitrator;
-import lejos.robotics.subsumption.Behavior;
 
 public class TestLightLevel {
 
-    static LightSensor sensor;
+	static final Port SENSOR_PORT = SensorPort.S1;
 
-    public static void main(String[] args) {
-        sensor = new LightSensor(SensorPort.S1);
-        Thread t = new Thread(sensor);
-        t.start();
-
-        Arbitrator a = new Arbitrator(new Behavior[] { new PrintLightLevel() }, true);
-        a.go();
-    }
-}
-
-class PrintLightLevel implements Behavior {
-
-    private boolean suppressed = false;
-
-    @Override
-    public boolean takeControl() {
-        return true;
-    }
-
-    @Override
-    public void suppress() {
-        suppressed = true; // standard practice for suppress methods
-    }
-
-    @Override
-    public void action() {
-        suppressed = false;
-        while (!suppressed) {
-            LCD.drawString("Light level: " + TestLightLevel.sensor.level, 20, 20);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	public static void main(String[] args) {
+		LightSensor sensor = new LightSensor();
+		Thread t = new Thread(sensor);
+		t.start();
+	}
 }
 
 class LightSensor implements Runnable {
 
-    float level;
-    private SampleProvider sp;
+	float level;
 
-    public LightSensor(Port port) {
-        EV3ColorSensor pro = new EV3ColorSensor(port);
-        sp = pro.getAmbientMode();
-    }
-
-    @Override
-    public void run() {
-        while (true) {
-            float[] sample = new float[sp.sampleSize()];
-            sp.fetchSample(sample, 0);
-            level = (int) sample[0];
-        }
-    }
+	@Override
+	public void run() {
+		EV3ColorSensor pro = new EV3ColorSensor(TestLightLevel.SENSOR_PORT);
+		SampleProvider sp = pro.getRedMode();
+		while (true) {
+			float[] sample = new float[sp.sampleSize()];
+			sp.fetchSample(sample, 0);
+			level = sample[0];
+			System.out.println(level);
+		}
+	}
 }
