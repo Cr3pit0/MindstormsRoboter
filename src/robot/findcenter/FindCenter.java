caba58860db481f27dcb2d2fb0be4526e9e335c1
Janik Brüll <j.bruell@topsystem.de>
2018-11-20 08:10:43 +0100

    [FindCenter] Use Threads to continuously update samples of sensors

--- src/robot/findcenter/FindCenter.java
+++ src/robot/findcenter/FindCenter.java
@@ -9,68 +9,109 @@ import robot.Pilot;
 
 public class FindCenter {
 
-	static final Port ULTRASONIC_SENSOR = SensorPort.S2;
-	static final Port GYRO_SENSOR = SensorPort.S3;
+    static final Port ULTRASONIC_SENSOR = SensorPort.S2;
+    static final Port GYRO_SENSOR = SensorPort.S3;
 
-	static SampleProvider ultrasonic;
-	static SampleProvider gyro;
+    public static void main(String[] args) throws InterruptedException {
+        Pilot pilot = new Pilot(50);
 
-	public static void main(String[] args) throws InterruptedException {
-		Pilot pilot = new Pilot(80);
+        GyroSensor gyro = new GyroSensor();
+        UltrasonicSensor ultrasonic = new UltrasonicSensor();
+        new Thread(gyro).start();
+        new Thread(ultrasonic).start();
 
-		EV3UltrasonicSensor pro = new EV3UltrasonicSensor(ULTRASONIC_SENSOR);
-		ultrasonic = pro.getDistanceMode();
-		
-		EV3GyroSensor gyroSensor = new EV3GyroSensor(GYRO_SENSOR);
-		gyro = gyroSensor.getAngleMode();
-		gyroSensor.reset();
-		
-		while (true) {
-			System.out.println(getCurrentAngle());
-		}
+        while (!(gyro.ready && ultrasonic.ready)) {
+            Thread.sleep(1000);
+        }
+        
+        // TEST START
+//        for (int i = 0; i < 8; i++) {
+//            while (gyro.angle < 90) {
+//                pilot.spinRight();
+//            }
+//            pilot.stop();
+//            gyro.reset();
+//        }
+//        System.exit(0);
+        // TEST END
 
-//		float[] distance = new float[4];
-//		for (int i = 0; i < 4; i++) {
-//			distance[i] = getCurrentDistance() * 100;
-//			Thread.sleep(2000);
-//			System.out.println(distance[i]);
-//			while (getCurrentAngle() < 90) {
-//				pilot.spinRight();
-//			}
-//			pilot.stop();
-//			gyroSensor.reset();
-////			pilot.rotateBy(90);
-//		}
-//		// Roboter steht wieder in Ausgangsposition
-//
-//		// TODO Roboter muss exakt auf der Stelle drehen!
-//		System.out.println((distance[0] - distance[2]) / 2);
-//		System.out.println((int) (distance[0] - distance[2]) / 2);
-//		System.out.println((distance[1] - distance[3]) / 2);
-//		System.out.println((int) (distance[1] - distance[3]) / 2);
-////		Thread.sleep(3000);
-//
-//		pilot.drive((int) ((distance[0] - distance[2]) / 2));
-//		while (getCurrentAngle() < 90) {
-//			pilot.spinRight();
-//		}
-////		pilot.rotateBy(90);
-//		pilot.stop();
-//		pilot.drive((int) ((distance[1] - distance[3]) / 2));
-//		pro.close();
-//		gyroSensor.close();
-	}
+        float[] distance = new float[4];
+        for (int i = 0; i < 4; i++) {
+            distance[i] = ultrasonic.distance * 100;
+            Thread.sleep(2000);
+            System.out.println(distance[i]);
+            while (gyro.angle < 90) {
+                pilot.spinRight();
+                System.out.println(gyro.angle);
+            }
+            pilot.stop();
+            gyro.reset();
+        }
+        // Roboter steht wieder in Ausgangsposition
 
-	private static float getCurrentDistance() {
-		float[] sample = new float[ultrasonic.sampleSize()];
-		ultrasonic.fetchSample(sample, 0);
-		return sample[0];
-	}
-	
-	private static float getCurrentAngle() {
-		float[] sample = new float[gyro.sampleSize()];
-		gyro.fetchSample(sample, 0);
-		System.out.println("Angle: " + sample[0]);
-		return Math.abs(sample[0]);
-	}
+        // TODO Roboter muss exakt auf der Stelle drehen!
+        System.out.println((distance[0] - distance[2]) / 2);
+        System.out.println((int) (distance[0] - distance[2]) / 2);
+        System.out.println((distance[1] - distance[3]) / 2);
+        System.out.println((int) (distance[1] - distance[3]) / 2);
+
+        pilot.drive((int) ((distance[0] - distance[2]) / 2));
+        while (gyro.angle < 90) {
+            pilot.spinRight();
+        }
+        pilot.stop();
+        pilot.drive((int) ((distance[1] - distance[3]) / 2));
+    }
+}
+
+class GyroSensor implements Runnable {
+
+    float angle;
+    boolean ready;
+    private EV3GyroSensor gyroSensor;
+    private SampleProvider sp;
+
+    GyroSensor() {
+        ready = false;
+    }
+
+    void reset() {
+        gyroSensor.reset();
+    }
+
+    @Override
+    public void run() {
+        gyroSensor = new EV3GyroSensor(FindCenter.GYRO_SENSOR);
+        sp = gyroSensor.getAngleMode();
+        gyroSensor.reset();
+        ready = true;
+        while (true) {
+            float[] sample = new float[sp.sampleSize()];
+            sp.fetchSample(sample, 0);
+            angle = Math.abs(sample[0]);
+        }
+    }
+}
+
+class UltrasonicSensor implements Runnable {
+
+    float distance;
+    boolean ready;
+    private SampleProvider sp;
+
+    UltrasonicSensor() {
+        ready = false;
+    }
+
+    @Override
+    public void run() {
+        EV3UltrasonicSensor pro = new EV3UltrasonicSensor(FindCenter.ULTRASONIC_SENSOR);
+        sp = pro.getDistanceMode();
+        ready = true;
+        while (true) {
+            float[] sample = new float[sp.sampleSize()];
+            sp.fetchSample(sample, 0);
+            distance = sample[0];
+        }
+    }
 }
